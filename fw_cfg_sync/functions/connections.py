611cc20fcf0113caa0d91d37c870bb8ea80f2d4a
Akellazzz <>
2022-01-04 19:45:22 +0300

    ok

--- fw_cfg_sync/functions/connections.py
+++ fw_cfg_sync/functions/connections.py
@@ -1,44 +1,75 @@
+'''Классы для взаимодействия с хостами.
+
+BaseConnection - базовый класс
+Multicontext - для взаимодействия с мультиконтекстными МСЭ Cisco ASA
+'''
 from datetime import datetime
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler, NetmikoTimeoutException
+from loguru import logger
 
 import os
 import sys
 import logging
 
-logging.basicConfig(filename="fw_cfg_sync/logs/netmiko.log", level=logging.DEBUG)
-logger = logging.getLogger("netmiko")
+# logging.basicConfig(filename="fw_cfg_sync/logs/netmiko.log", level=logging.DEBUG)
+# logger = logging.getLogger("netmiko")
 
+# logger.configure(**log_config)
 
 class BaseConnection:
+    '''Базовый класс'''
+
     def __init__(self, **kwargs):
         # self.__dict__.update(kwargs)
         self.name = kwargs.get("name") if kwargs.get("name") else kwargs.get("conn").get("host")
         self.conn = kwargs.get("conn")
+        self.is_reachable: bool
         self.conn["allow_auto_change"] = False
-        self.errors = []
+        
 
+    @logger.catch
+    def check_reachability(self):
+        '''Проверка возможности подключения к устройству'''
+
+        try:
+            with ConnectHandler(**self.conn) as net_connect:
+                self.is_reachable = True
+                logger.debug(f'Подключение к устройству {self.name} успешно')
+
+        except NetmikoTimeoutException as e:
+            self.is_reachable = False
+            logger.error(f'TCP connection to {self.name} failed')
+            
+
+    @logger.catch
     def send_command(self, command: str):
+        logger.debug(f'Отправка команды {command} устройству {self.name}')
+
         try:
             with ConnectHandler(**self.conn) as net_connect:
                 return net_connect.send_command(command) 
-        except Exception as e:
-            self.errors.append(e)
+        except NetmikoTimeoutException as e:
+            logger.error(f'TCP connection to {self.name} failed')
 
+
+    @logger.catch
     def send_cfg_from_file(self, cfg_file: str):
+        logger.debug(f'Отправка устройству {self.name} команд из файла {cfg_file}')
         try:
             with ConnectHandler(**self.conn) as net_connect:
-                r = net_connect.send_config_from_file(cfg_file) 
-        except Exception as e:
-            self.errors.append(e)
-        return r
-
+                return net_connect.send_config_from_file(cfg_file) 
+        except NetmikoTimeoutException as e:
+            logger.error(f'TCP connection to {self.name} failed')
 
 class Multicontext(BaseConnection):
+    '''Класс для взаимодействия с мультиконтекстными МСЭ Cisco ASA '''
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # self.__dict__.update(kwargs)
         self.contexts = []
 
+    @logger.catch
     def get_contexts(self):
         contexts = []
         with ConnectHandler(**self.conn) as net_connect:
@@ -52,28 +83,31 @@ class Multicontext(BaseConnection):
                 contexts.append(context)   
         self.contexts = dict.fromkeys(contexts)
 
+
+    @logger.catch
     def send_command_to_context(self, command: str, context: str = ""):
         try:
             with ConnectHandler(**self.conn) as net_connect:
                 if context:
                     net_connect.send_command(f"changeto context {context}") 
                 return net_connect.send_command(command) 
-        except Exception as e:
-            self.errors.append(e)
+        except NetmikoTimeoutException as e:
+            logger.error(f'TCP connection to {self.name} failed')
+
 
     def get_context_backup(self, context):
-        ''' Забирает бэкап конфигурации контекста и сохраняет в self.contexts[context]['config']
-        Если хост недоступен, сохраняет ошибку в self.contexts[context]['errors']'''
+        ''' Забирает бэкап конфигурации контекста и сохраняет в self.contexts[context]['config']'''
+
         result = self.send_command_to_context(command = "show run", context = context)
         if result and result.endswith(": end"):
             self.contexts[context] = result
         else:
-            print(f"Unable to get config from {context}")
-            self.errors.append(result)
-            self.contexts[context]['errors']
+            logger.error(f'Unable to get config from {self.name} - {context}')
 
-    def save_run_cfg_locally(self, context):
+
+    def save_backup_to_file(self, context):
         ''' Сохраняет бэкап конфигурации в файл {имя_МСЭ}_{контекст}_{время}.txt'''
+
         d = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         # dirname = os.path.join(os.path.dirname(__file__), "fw_configs") 
         filename = self.name + '_' + context + '_' + d + '.txt'
--- fw_cfg_sync/netm.py
+++ fw_cfg_sync/netm.py
@@ -1,44 +1,67 @@
-from datetime import datetime
-from netmiko import ConnectHandler
+import keyring
 
-from functions.connections import Multicontext
 import os
+import sys
 import pickle
 import logging
+from functions.connections import Multicontext
 from functions.load_config import load_config
+from loguru import logger
 
-logging.basicConfig(filename="fw_cfg_sync/logs/netmiko.log", level=logging.DEBUG)
-logger = logging.getLogger("netmiko")
+log_config = {
+    "handlers": [
+        {"sink": sys.stdout},
+        {"sink": "fw_cfg_sync/logs/fw_cfg_sync_{time}.log", 
+        "retention": "30 days", 
+        'backtrace': True, 
+        'diagnose': True},
+    ]
+}
+logger.configure(**log_config)
+
+# logger.add("fw_cfg_sync/logs/fw_cfg_sync_{time}.log", retention="30 days", backtrace=True, diagnose=True)
+logger.debug("That's it, beautiful and simple logging!")
+# logging.basicConfig(filename="fw_cfg_sync/logs/netmiko.log", level=logging.DEBUG)
+# logger = logging.getLogger("netmiko")
 
 
 def set_roles(inv):
     # TODO
     # active = Multicontext(conn = inv.devices["fw_a"]["connection"], name = inv.devices["fw_a"].get("name"))
     active = Multicontext(conn = inv.devices["fw_a"]["connection"])
-    standby = Multicontext(conn = inv.devices["fw_a"]["connection"], name = inv.devices["fw_a"].get("name"))
+    standby = Multicontext(conn = inv.devices["fw_b"]["connection"], name = inv.devices["fw_b"].get("name"))
     # standby = Multicontext(conn = inv.devices["fw_b"]["connection"])
     return active, standby
 
 
-# dirname = os.path.dirname(__file__)
-# p = os.path.join(dirname, "inventory", "multicontext.yaml")
-# inv = load_config(p)
-# active, standby = set_roles(inv)
-# active.get_contexts()
-# for context in active.contexts:
-#     active.get_context_backup(context)
-#     active.save_backup_to_file(context)
+dirname = os.path.dirname(__file__)
+p = os.path.join(dirname, "inventory", "multicontext.yaml")
+inv = load_config(p)
+active, standby = set_roles(inv)
+for device in active, standby:
+    device.check_reachability()
+    if not device.is_reachable:
+        pass
+        # mail() #TODO
 
-with open("fw_cfg_sync\\tests\\fw_configs\\pickle_dumps\\active_with_backups.pickle", 'rb') as f:
-    active = standby = pickle.load(f)
+active.get_contexts()
+for context in active.contexts:
+    active.get_context_backup(context)
+    active.save_backup_to_file(context)
+
+# with open("fw_cfg_sync\\tests\\fw_configs\\pickle_dumps\\active_with_backups2.pickle", 'wb') as f:
+#     pickle.dump(active, f)
+# with open("fw_cfg_sync\\tests\\fw_configs\\pickle_dumps\\standby_unreacheble.pickle", 'wb') as f:
+#     pickle.dump(standby, f)
+
+# with open("fw_cfg_sync\\tests\\fw_configs\\pickle_dumps\\active_with_backups2.pickle", 'rb') as f:
+#     active = pickle.load(f)
+# with open("fw_cfg_sync\\tests\\fw_configs\\pickle_dumps\\standby_unreacheble.pickle", 'rb') as f:
+#     standby = pickle.load(f)
 
 
-cmd = "sh int ip br"
-for fw in (active, standby):
-    result = fw.send_command(cmd)
-    if result:
-        print(result)
-    else:
-        print(fw.errors)
 
-pass
+# keyring.set_password("fw1", "aaa", "aaa")
+
+p = keyring.get_password("fw1", "aaa")
+print(p)
