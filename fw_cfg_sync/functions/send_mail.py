3e53c5cf5fa1a5ab08dd7dd288be321bc63c58fe
Akellazzz <>
2022-01-20 19:42:15 +0300

    .

--- fw_cfg_sync/functions/send_mail.py
+++ fw_cfg_sync/functions/send_mail.py
@@ -1,54 +1,61 @@
-from zipfile import ZipFile
-
-import datetime
-import os
 import smtplib
 from email.mime.text import MIMEText
-from contextlib import closing
-from sys import argv, exit
-
-os.chdir(app.config["UPLOAD_FOLDER"])
-with ZipFile("result.zip", "w") as zipObj:
-    zipObj.write("report.csv")
-    zipObj.write(res_filename)
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from os.path import basename
 
 
-# EMAIL
-HOST = "smtp.vtb.ru"
-FROM = "eekosyanenko@vtb.ru"
-SUBJECT = "Результат синхронизации конфигураций МСЭ"
-ADMINS = "eekosyanenko@vtb.ru"
+def send_mail(text: str,
+              subject: str = "Отчет о синхронизации конфигураций МСЭ",
+              send_from: str = "eekosyanenko@vtb.ru",
+              send_to: list[str] = ["eekosyanenko@vtb.ru"],
+              server: str = "smtp.region.vtb.ru",
+              files: list[str] = None) -> None :
+    """Compose and send email with provided info and attachments.
 
+    Args:
+        text: message
+        subject: message title
+        send_from: from name
+        send_to: to name(s)       
+        server: mail server host name
+        files: list of file paths to be attached to email
+    """
+    
+    assert isinstance(send_to, list)
 
-def alert(alert_text, to=ADMINS):
+    message = MIMEMultipart()
+    
+    message["From"] = send_from
+    message["To"] = ', '.join(send_to)
+    message["Subject"] = subject
 
     html = f"""
     <html>
     <head></head>
     <body>
-        <p>{alert_text}
+        <p>{text}
         </p>
     </body>
     </html>
     """
-    message = MIMEText(html, "html")
-    message["From"] = FROM
-    # message["To"] = TO
-    message["To"] = to
-    message["Subject"] = SUBJECT
+    message.attach(MIMEText(html, "html"))    
+
+    for f in files or []:
+        with open(f, "rb") as fil:            
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+            
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        
+        message.attach(part)
 
     msg_full = message.as_string()
 
-    server = smtplib.SMTP(HOST)
-    # server.sendmail(FROM, [TO], msg_full)
-    server.sendmail(FROM, [to], msg_full)
+    server = smtplib.SMTP(server)
+    server.sendmail(send_from, send_to, msg_full)
     server.quit()
-
-
-alert_text += host + "<br>" + show_result.replace("\n", "<br>") + "<br><br>"
-# Общая рассылка:
-alert(alert_text, to=LIST)
-
-# Расширенная рассылка админам:
-adm_alert_text = alert_text + f"<br>Будут выполнены команды: <br>"
-alert(adm_alert_text, to=LIST)
+    
+# send_mail("test", files = ["del.txt"])
